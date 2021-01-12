bee6aba1850e9df028aa2d2d9359c007268d5cdb
Niklas Esch <niklas@esch-online.eu>
2021-01-12 13:29:29 +0100

    add lines in prognosis graph for both scenarios where the co2 budget will be depleted

--- covid-climate/src/app/prognosis-graph/prognosis-graph.component.ts
+++ covid-climate/src/app/prognosis-graph/prognosis-graph.component.ts
@@ -83,7 +83,9 @@ export class PrognosisGraphComponent implements OnInit, AfterViewInit, OnChanges
   ngAfterViewInit(): void {
     this.prognosisGraphSvg = this.prognosisGraph.nativeElement;
     this.initPrognosisGraph();
+    this.initCo2BudgetLines();
     this.updatePrognosisGraph();
+    this.updateCo2BudgetLines();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -168,12 +170,12 @@ export class PrognosisGraphComponent implements OnInit, AfterViewInit, OnChanges
 
     this.prognosisSvg.selectAll('#xAxis')
       .transition()
-      .duration(1000)
+      .duration(1)
       .call(this.xAxis as any);
 
     this.prognosisSvg.selectAll('#yAxis')
       .transition()
-      .duration(1000)
+      .duration(1)
       .call(this.yAxis as any);
   }
 
@@ -184,7 +186,7 @@ export class PrognosisGraphComponent implements OnInit, AfterViewInit, OnChanges
     lineHistoric.enter()
       .merge(lineHistoric as any)
       .transition()
-      .duration(1000)
+      .duration(1)
       .attr('d', this.lineHistoric);
 
     const linePrognosisLockdown = this.prognosisSvg
@@ -194,7 +196,7 @@ export class PrognosisGraphComponent implements OnInit, AfterViewInit, OnChanges
     linePrognosisLockdown.enter()
       .merge(linePrognosisLockdown as any)
       .transition()
-      .duration(1000)
+      .duration(1)
       .attr('d', this.linePrognosisLockdown);
 
     const linePrognosisNoLockdown = this.prognosisSvg
@@ -204,18 +206,92 @@ export class PrognosisGraphComponent implements OnInit, AfterViewInit, OnChanges
     linePrognosisNoLockdown.enter()
       .merge(linePrognosisNoLockdown as any)
       .transition()
-      .duration(1000)
+      .duration(1)
       .attr('d', this.linePrognosisNoLockdown);
+  }
+
+  private initCo2BudgetLines(): void {
+    this.sliderSvg.append('svg:line')
+      .attr('class', 'budgetLine')
+      .attr('id', 'budgetLineLockdowns')
+      .attr('y1', 0)
+      .attr('y2', this.height)
+    this.sliderSvg.append('svg:line')
+      .attr('class', 'budgetLine')
+      .attr('id', 'budgetLineNoLockdowns')
+      .attr('y1', 0)
+      .attr('y2', this.height)
+  }
+
+  private updateCo2BudgetLines(): void {
+    const lockdowns = true;
+
+    const depletionYearLockdowns = this.x(this.calculateDepletionYear(lockdowns));
+    this.sliderSvg
+      .select('#budgetLineLockdowns')
+      .attr('x1', depletionYearLockdowns)
+      .attr('x2', depletionYearLockdowns)
+
+    const depletionYearNoLockdowns = this.x(this.calculateDepletionYear(!lockdowns));
+    this.sliderSvg
+      .select('#budgetLineNoLockdowns')
+      .attr('x1', depletionYearNoLockdowns)
+      .attr('x2', depletionYearNoLockdowns)
+  }
+
+  private calculateDepletionYear(lockdowns: boolean): number {
+    const prognosisData = this.dataService.getHistoricCo2Data({
+      prognosisDataFilter: [PrognosisDataIndicators.prognosis],
+    });
+    const upcomingYears = prognosisData.map(dp => dp.year);
+    const remainingBudget = 1042800; //remaining mtons of co2 for 2°C scenario
+    //const remainingBudget = 293000; //remaining mtons of co2 for 1,5°C scenario
+    var depletionYear, co2Emissions;
+
+    if(lockdowns) {
+      co2Emissions = prognosisData.map(dp => dp.co2PrognosisLockdown);
+    } else {
+      co2Emissions = prognosisData.map(dp => dp.co2PrognosisNoLockdown);
+    }
+
+    var usedBudget = 0, i;
+    for(i = 0; usedBudget < remainingBudget; i++){
+      usedBudget = usedBudget + (Number(co2Emissions[i]) * 365.25); //to account for leap years
+    }
+
+    if(i <= 30) {
+      depletionYear = upcomingYears[i - 1];
+    } else {
+      depletionYear = upcomingYears[29]; //year 2050: the last datapoint entry
+      console.log('CO2 budget will be depleted some time after 2050.');
+    }
+    return depletionYear;
+
+    /*without using our data and simply sticking to the mcc carbon clock source we would have:
+    //1.5°C scenario
+    if(lockdowns){
+      return 2027.5 //for 1.5°C scenario with lockdowns (38.341mtons ( = 91,29% of 42000mtons) CO2 annually, divided to the remaining 293000mtons)
+    } else {
+      return 2027 //for 1.5°C scenario without lockdowns (42000mtons CO2 annually, divided to the remaining 293000mtons)
+    }
+    //2°C scenario
+    if(lockdowns) {
+      return 2047 //for 2°C scenario with lockdowns (38.341mtons ( = 91,29% of 42000mtons) CO2 annually, divided to the remaining 1042800mtons)
+    } else {
+      return 2045 //for 2°C scenario without lockdowns (42000mtons CO2 annually, divided to the remaining 1042800mtons)
+    }*/
   }
 
   // slider onChange low
   valueChange(value: number): void {
     this.sliderLowValue = value;
     this.updatePrognosisGraph();
+    this.updateCo2BudgetLines();
   }
   // slider onChange high
   valueChangeHigh(highValue: number): void {
     this.sliderHighValue = highValue;
     this.updatePrognosisGraph();
+    this.updateCo2BudgetLines();
   }
 }
